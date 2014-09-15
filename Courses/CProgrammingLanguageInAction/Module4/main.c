c74aba1c7ced922b2d6b8389064106cfe45214c4
Nemesek <dnemesek@gmail.com>
2014-09-14 19:49:08 -0500

    finished module4

--- Courses/CProgrammingLanguageInAction/Module4/main.c
+++ Courses/CProgrammingLanguageInAction/Module4/main.c
@@ -1,14 +1,15 @@
 // start of main.c
 #include "multiply.h"
-
+#include <stdio.h>
 #define LEVEL 2
 
 int main()
 {
-	multiply(4,5) /* and then */ multiply(6,7);
-	#if LEVEL > 0
-	SQUARE(9);
-	#endif
+	//multiply(4,5) /* and then */ multiply(6,7);
+	// #if LEVEL > 0
+	// SQUARE(9);
+	// #endif
+	printf("4 * 5 = %d\n", multiply(4,5));
 }
 
 // end of main.c
